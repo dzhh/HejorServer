d8f5c954223b5e3f45b6ad07f1e6397e94ce1939
DESKTOP-5HNJ83V\fly <dzhh2011@163.com>
2017-05-02 15:22:01 +0800

    定时任务

---
+++ src/com/fly/task/TaskOne.java
@@ -0,0 +1,21 @@
+package com.fly.task;
+
+import org.quartz.JobExecutionContext;  
+import org.quartz.JobExecutionException;  
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class TaskOne extends QuartzJobBean {
+
+	private int timeout;  
+	
+	public void setTimeout(int timeout) {  
+        this.timeout = timeout;  
+    }
+	
+	@Override
+	protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
+		System.out.println("-----定时任务执行-----");
+	}
+
+
+}
